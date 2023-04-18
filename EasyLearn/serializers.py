cdf3656815922ee9847c45d488923cbc333196db
Ioann <isakov-ioann@gmailcom>
2023-04-18 10:03:47 +0600

    change user

--- EasyLearn/serializers.py
+++ EasyLearn/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 from EasyLearn.models import Post
 from .models import Category
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
+        
 
 class PostSerializer(serializers.ModelSerializer):
 
--- EasyLearn/views.py
+++ EasyLearn/views.py
@@ -5,6 +5,17 @@ from rest_framework import generics
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 
 class PostListView(ListCreateAPIView):
